6c9333e7101f831c95a2c1440caa8b8a6dfb18c7
vahghar <raghav10g@gmail.com>
2025-06-03 03:04:30 +0530

    backend-done

--- backend/.venv/app/core/config.py
+++ backend/.venv/app/core/config.py
@@ -1,19 +1,22 @@
 import os
 from pydantic_settings import BaseSettings
 from typing import ClassVar
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     ENV: str = "development"
-    DEBUG: bool = "True"
+    DEBUG: bool = True
     
     # Database
-    DATABASE_URL: str = "postgresql://neondb_owner:npg_DfwBQz6OoUF1@ep-wispy-frost-a8nbhxcx-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
+    DATABASE_URL: str = os.getenv("DATABASE_URL","")
     
     # Celery
-    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
-    CELERY_RESULT_BACKEND: str = "redis://localhost:6379/1"
+    #CELERY_BROKER_URL: str = "redis://localhost:6379/0"
+    #CELERY_RESULT_BACKEND: str = "redis://localhost:6379/1"
     
-    SECRET_KEY: ClassVar[str] = "kS9$h4Xw3BvT!mC7uL@zPf2R^gQxD*Nj"
+    SECRET_KEY: str = os.getenv("SECRET_KEY","eepyuppie")
     ACCESS_TOKEN_EXPIRE_MINUTES: ClassVar[int] = 60
     ALGORITHM: ClassVar[str] = "HS256"
     
